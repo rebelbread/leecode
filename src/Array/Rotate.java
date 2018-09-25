580ff3e81349c643b6315ee4e085c33022c7659b
redbread <314581947@qq.com>
2018-09-25 21:08:06 +0800

    some training

--- src/Array/Rotate.java
+++ src/Array/Rotate.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.Arrays;
+
 /**
  * @author zhiwj
  * @date 2018/9/25
@@ -31,9 +33,32 @@ public class Rotate {
      */
 
     public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        new Rotate().rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
     }
 
     public void rotate(int[] nums, int k) {
 
+        k = k % nums.length;
+
+        for (int i = 0, j = nums.length - k - 1; i < j; i++, j--) {
+            swap(nums, i, j);
+        }
+
+        for (int i = nums.length - k, j = nums.length - 1; i < j; i++, j--) {
+            swap(nums, i, j);
+        }
+
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            swap(nums, i, j);
+        }
+
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
